2f82dfe6eff610cc05840563a599e53e60907f3e
joseph-rato <joseph.rato@gmail.com>
2018-09-11 18:13:14 -0700

    start custom validations for CatRentalRequest

--- nine9cats/app/models/cat_rental_request.rb
+++ nine9cats/app/models/cat_rental_request.rb
@@ -1,23 +1,21 @@
 class CatRentalRequest < ApplicationRecord
   
   validates :cat_id, :start_date, :end_date, :status, presence: true 
+  validate :overlapping_requests
   
   belongs_to :cat,
     foreign_key: :cat_id,
     class_name: 'Cat'
     
-  def over_lapping_requests
-    results = CatRentalRequest.where("cat_id = #{self.cat_id} AND id != #{self.id}") 
-    
-    results.each do |request|
-      if request.end_date > self.start_date
-         self.errors.full_messages << "Overlapping Requests"
-         raise self.errors.full_messages
-      else
-         next
-      end  
-    end 
-    self
+  def overlapping_requests
+    CatRentalRequest
+        .where("cat_id = #{self.cat_id} AND id != #{self.id}") 
+        .where.not("end_date <= #{self.start_date} OR start_date >= #{self.end_date}")
   end 
+  
+  def overlapping_approved_requests
+    overlapping_requests.where(status: 'APPROVED')
+    
+  end
   
 end 
